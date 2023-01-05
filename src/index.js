d2444b60f439dee2d5c151599c1ac607f70006fc
Kataiii <99545244+Kataiii@users.noreply.github.com>
2023-01-05 14:51:17 +0400

    remake for jsx

--- src/index.js
+++ src/index.js
@@ -3,31 +3,50 @@ import ReactDOM from 'react-dom/client';
 //import './index.css';
 //import App from './App';
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+//const root = ReactDOM.createRoot(document.getElementById("root"));
 const numbers = [1,2,3,4,5]
+
 const Book = (props) => {
   console.log(props);
-  return React.createElement('div', {}, [
-    React.createElement('h2', {key:0}, props.name),
-    React.createElement('p', {key:1}, props.year),
-    React.createElement('p', {key:2}, props.price),
-  ]);
+  return <div>
+    <h2 key={0}>{props.name}</h2>
+    <p key={1}>{props.year}</p>
+    <p key={2}>{props.price}</p>
+  </div>
 }
+// const Book = (props) => {
+//   console.log(props);
+//   return React.createElement('div', {}, [
+//     React.createElement('h2', {key:0}, props.name),
+//     React.createElement('p', {key:1}, props.year),
+//     React.createElement('p', {key:2}, props.price),
+//   ]);
+// }
 
+//TO DO jsx
 const App = () => {
-  return React.createElement('div', {}, [
-    React.createElement('h1', {key:5,id: 'hello', className: 'class1'}, 'Hello world!'),
-    React.createElement(Book, {key:6, name: 'JS', year: 2018, price: 1000}), 
-    React.createElement(Book, {key:7, name: 'Java', year: 2010, price: 500}),
-    React.createElement(Book, {key:8, name: 'Python', year: 2018, price: 1000})
-  ]);
+  return <div>
+    <h1 key='5' id='hello' className='class1'>Hello world</h1>
+    <Book key={6} name='JS' year={2018} price={1000}/>
+    <Book key={7} name='Java' year={2010} price={500}/>
+    <Book key={8} name='Python' year={2018} price={1000}/>
+  </div>
 }
 
-root.render(React.createElement(App));
+// const App = () => {
+//   return React.createElement('div', {}, [
+//     React.createElement('h1', {key:5,id: 'hello', className: 'class1'}, 'Hello world!'),
+//     React.createElement(Book, {key:6, name: 'JS', year: 2018, price: 1000}), 
+//     React.createElement(Book, {key:7, name: 'Java', year: 2010, price: 500}),
+//     React.createElement(Book, {key:8, name: 'Python', year: 2018, price: 1000})
+//   ]);
+// }
 
-// const root = ReactDOM.createRoot(document.getElementById('root'));
-// root.render(
-//   <React.StrictMode>
-//     <App />
-//   </React.StrictMode>
-// );
+//root.render(React.createElement(App));
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
