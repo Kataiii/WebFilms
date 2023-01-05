33c5601c6c6d17d7a2291455f678d881c97016db
Kataiii <99545244+Kataiii@users.noreply.github.com>
2023-01-05 15:15:36 +0400

    division into modules

---
+++ src/Book.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Book = (props) => {
+    console.log(props);
+    return (
+        <div>
+            <h2 key={0}>{props.name}</h2>
+            <p key={1}>{props.year}</p>
+            <p key={2}>{props.price}</p>
+        </div>
+    );
+};
+
+export default Book;
